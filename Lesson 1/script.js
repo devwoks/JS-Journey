36909f95284e86a1952753a83ed61e745256f67d
devwoks <166159602+devwoks@users.noreply.github.com>
2025-02-15 13:11:40 +0000

    Add user input and result display in Lesson 1

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -1,6 +1,6 @@
 document.getElementById("myButton").addEventListener("click", function() {
     let name = document.getElementById("userName").value;
-    document.getElementById("output").innerHTML = "Hello, " + name + "!";
+    document.getElementById("result").innerHTML = "Hello, " + name + "!";
   });
 
   console.log('Script is linked properly');
