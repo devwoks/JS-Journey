b7972da6d195576c72a3888c6b444b83d6a64708
devwoks <166159602+devwoks@users.noreply.github.com>
2025-02-15 13:05:23 +0000

    Add console log to confirm script linkage in Lesson 1

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -2,3 +2,5 @@ document.getElementById("myButton").addEventListener("click", function() {
     let name = document.getElementById("userName").value;
     document.getElementById("output").innerHTML = "Hello, " + name + "!";
   });
+
+  console.log('Script is linked properly');
